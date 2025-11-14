d0d44ddf6a5b2d8ebb16b49be2d3e1b73713ab81
ziidan <azzidanirsyad@gmail.com>
2025-11-14 11:03:34 +0700

    ini

---
+++ database/migrations/2025_11_14_040053_add_id_pengaduan_to_temporary_item_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('temporary_item', function (Blueprint $table) {
+        $table->unsignedBigInteger('id_pengaduan')->after('id_user');
+    });
+}
+
+public function down()
+{
+    Schema::table('temporary_item', function (Blueprint $table) {
+        $table->dropColumn('id_pengaduan');
+    });
+}
+
+};
